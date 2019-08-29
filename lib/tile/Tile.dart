38b677510d305cc15714ea66f24011c944fe2b98
plohkoon <greg.huber@outlook.com>
2019-08-28 21:58:47 -0700

    Changed Tile to a stateful widget, added logic to render the player path on any given tile

--- lib/tile/Tile.dart
+++ lib/tile/Tile.dart
@@ -1,9 +1,83 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 
-class Tile extends StatelessWidget {
+class Tile extends StatefulWidget {
   final Map<String, bool> directions;
   Tile({Key key, @required this.directions}): super(key: key);
+
+  _TileState createState() => _TileState(directions: directions);
+}
+
+class _TileState extends State<Tile> {
+  //defines things being passed to the state
+  Map<String, bool> directions;
+  //constructor
+  _TileState({@required this.directions});
+  //3 lists to define the look of the player path in the tile
+  List<Alignment> tileAlignment = new List<Alignment>.generate(2, (i) => Alignment.center);
+  List<double> widths = new List<double>.generate(2, (i) => 0);
+  List<double> heights = new List<double>.generate(2, (i) => 0);
+  //initiates the state
+  @override
+  void initState() {
+    this._updateMoves();
+    super.initState();
+  }  
+  //on tile update updates the player path
+  @override
+  void didUpdateWidget(Tile oldWidget) {
+    if(oldWidget.directions != this.widget.directions) {
+      this.directions = this.widget.directions;
+      this._updateMoves();
+    }
+    super.didUpdateWidget(oldWidget);
+  }
+  //function to update the player path on the tile
+  void _updateMoves() {
+    //creates new instances of the list
+    List<Alignment> tileAlignment = new List<Alignment>.generate(2, (i) => Alignment.center);
+    List<double> widths = new List<double>.generate(2, (i) => 0);
+    List<double> heights = new List<double>.generate(2, (i) => 0);
+    //initiates a count
+    int count = 0;
+    /*
+      These if statements stylize the paths,
+      the try catch ensures it caps at 2 paths
+    */
+    try {
+      if(directions["moveUp"]) {
+        tileAlignment[count] = Alignment.topCenter;
+        widths[count] = 0.2;
+        heights[count] = 0.6;
+        count++;
+      }
+      if(directions["moveLeft"]) {
+        tileAlignment[count] = Alignment.centerLeft;
+        widths[count] = 0.6;
+        heights[count] = 0.2;
+        count++;
+      }
+      if(directions["moveDown"]) {
+        tileAlignment[count] = Alignment.bottomCenter;
+        widths[count] = 0.2;
+        heights[count] = 0.6;
+        count++;
+      }
+      if(directions["moveRight"]) {
+        tileAlignment[count] = Alignment.centerRight;
+        widths[count] = 0.6;
+        heights[count] = 0.2;
+        count++;
+      }
+    }
+    catch(error) {
+      print("there's too many directions for a path");
+    }
+    //sets the values into the state
+    this.tileAlignment = tileAlignment;
+    this.widths = widths;
+    this.heights = heights;
+  }
 
   //builds the widget
   @override
@@ -62,6 +136,28 @@ class Tile extends StatelessWidget {
           top: 0,
           left: 0,
         ),
+        //box to render part of the path on the tile
+        Positioned.fill(
+          child: FractionallySizedBox (
+            alignment: this.tileAlignment[0],
+            heightFactor: this.heights[0],
+            widthFactor: this.widths[0],
+            child: Container(
+              color: Theme.of(context).primaryColorDark,
+            ),
+          ),
+        ),
+        //box to render other part of the path on the tile
+        Positioned.fill(
+          child: FractionallySizedBox (
+            alignment: this.tileAlignment[1],
+            heightFactor: this.heights[1],
+            widthFactor: this.widths[1],
+            child: Container(
+              color: Theme.of(context).primaryColorDark,
+            ),
+          ),
+        )
       ],
     );
   }
