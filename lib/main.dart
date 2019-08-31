46742115f87ed0e34fa7d1ccdaf22b89ba6bfe35
plohkoon <greg.huber@outlook.com>
2019-08-30 21:59:51 -0700

    Changed how main page gets rendered to support the accessibility switch and night mode button

--- lib/main.dart
+++ lib/main.dart
@@ -6,13 +6,8 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Maze Game',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        primaryColor: Colors.red,
-      ),
-      home: MyHomePage(title: 'Maze Game'),
+    return MyHomePage(
+      title: "Maze Game"
     );
   }
 }
@@ -30,6 +25,7 @@ class _MyHomePageState extends State<MyHomePage> {
   //initializes the state
   int size = 5;
   bool accessibleControls = false;
+  bool darkMode = false;
   final String title;
   //constructor
    _MyHomePageState(this.title);
@@ -39,63 +35,99 @@ class _MyHomePageState extends State<MyHomePage> {
       this.size++;
     });
   }
-
+  //function for the FAB to toggle darkmode
+  void toggleDarkMode() {
+    setState(() {
+      this.darkMode = !this.darkMode;
+    });
+  }
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        //appBar that contains the title and accessibility controls
-        appBar: AppBar(
-          title: Row(
-            children: [
-              Text(this.title),
-              Expanded(child: Row(),),
-              //renders accessibility controls label and a switch to toggle it
-              Row(
-                children: [
-                  Text("Accessibility Controls"),
-                  Switch(
-                    value: accessibleControls,
-                    onChanged: (bool newVal) => {
-                      setState(() {
-                        this.accessibleControls = !this.accessibleControls;
-                      }),
-                    },
+    return MaterialApp(
+      title: this.title,
+      //theme data toggles with dark mode toggle
+      theme: darkMode ? 
+        ThemeData(
+          primarySwatch: Colors.grey,
+          primaryColor: Colors.deepOrange,
+          scaffoldBackgroundColor: Colors.black
+        ):
+        ThemeData(
+          primarySwatch: Colors.blue,
+          primaryColor: Colors.red,
+        ),
+      home: SafeArea(
+        child: Scaffold(
+          //appBar that contains the title and accessibility controls
+          appBar: AppBar(
+            title: Row(
+              children: [
+                Text(this.title),
+                Expanded(child: Row(),),
+                //renders accessibility controls label and a switch to toggle it
+                Row(
+                  children: [
+                    Text("Accessibility Controls"),
+                    Switch(
+                      value: accessibleControls,
+                      onChanged: (bool newVal) {
+                        setState(() {
+                          this.accessibleControls = !this.accessibleControls;
+                        });
+                      },
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          //the maze is the body of the app
+          body: Maze(
+            size: this.size,
+            accessibleControls: this.accessibleControls,
+            triggerWin: this.triggerWin,
+          ),
+          //FAB to change from light to dark mode
+          floatingActionButton: FloatingActionButton(
+            //passes the function
+            onPressed: this.toggleDarkMode,
+            //changes the icon depending on if we are in dark mode
+            child: this.darkMode ?
+            Icon(
+              Icons.brightness_3
+            ):
+            Icon(
+              Icons.brightness_5,
+            ),
+          ),
+          //will eventually be a navBar to switch between modes
+          bottomNavigationBar: BottomAppBar(
+            child: Row(
+              //equally spacing the buttons on the navBar for navigation
+              children: <Widget>[
+                Expanded(
+                  child: IconButton(
+                    icon: Icon(Icons.home),
+                    onPressed: () => print("home"),
                   ),
-                ],
-              ),
-            ],
+                ),
+                Expanded(
+                  child: IconButton(
+                    icon: Icon(Icons.map),
+                    onPressed: () => print("maze1"),
+                  ),
+                ),
+                Expanded(
+                  child: IconButton(
+                    icon: Icon(Icons.playlist_add_check),
+                    onPressed: () => print("maze2"),
+                  ),
+                )
+              ],
+            ),
           ),
         ),
-        //the maze is the body of the app
-        body: Maze(
-          size: this.size,
-          accessibleControls: this.accessibleControls,
-          triggerWin: this.triggerWin,
-        ),
-        //just a test FAB that tests the maze resizing
-        floatingActionButton: FloatingActionButton(
-          onPressed: ()=>setState(() => size++),
-          child: Icon(
-            Icons.add,
-          ),
-        ),
-        //will eventually be a navBar to switch between modes
-        bottomNavigationBar: BottomAppBar(
-          child: Row(
-            children: <Widget>[
-              IconButton(
-                icon: Icon(Icons.home),
-                onPressed: () => print("home"),
-              ),
-              IconButton(
-                icon: Icon(Icons.map),
-                onPressed: () => print("maze1"),
-              )
-            ],
-          )
-        ),
-      )
+      ),
     );
   }
 }
